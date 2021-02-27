587b6d1ddc25f05e425ea78596df9da939af76a7
kagol <kagol@sina.com>
2021-02-27 23:58:24 +0800

    refactor(accordion): remove comment and add todo

--- devui/accordion/accordion.tsx
+++ devui/accordion/accordion.tsx
@@ -54,33 +54,5 @@ export default defineComponent({
         </AccordionList>
       </div>
     }
-
-    // return () => {
-    //   return (!innerListTemplate || deepth === 0) ? <ul class="devui-accordion-list">
-    //     { data.map(category => {
-    //       return <li class="devui-accordion-item" key={category.title}>
-    //         <div
-    //           class={`devui-accordion-item-title devui-over-flow-ellipsis ${open?'open':''} ${childActived?'active':''} ${disabled?'disabled':''}`}
-    //           title={category.title}
-    //           onClick={toggle}
-    //         >
-    //           <div
-    //             class={`devui-accordion-splitter ${deepth === 0?'devui-parent-list':''}`}
-    //             style={{ left: deepth * 20 + 10 + 'px' }}
-    //           ></div>
-    //           { category.title }
-    //           <AccordionList {...props} data={data as any}></AccordionList>
-    //         </div>
-    //         <ul class="devui-accordion-submenu">
-    //           { category.children.map(component => {
-    //             return <li class="devui-accordion-item" key={component.title}>
-    //               <router-link to={component.link}>{ component.title }</router-link>
-    //             </li>
-    //           })}
-    //         </ul>
-    //       </li>
-    //     }) }
-    //   </ul> : null
-    // }
   }
 })
