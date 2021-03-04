177fb0af084892a497db5ecfd7b7b7b4baaab497
kagol <kagol@sina.com>
2021-03-04 21:23:18 +0800

    feat: add transfer api scripts and improve api

--- devui/back-top/demo/back-top.route.ts
+++ devui/back-top/demo/back-top.route.ts
@@ -1,12 +1,14 @@
 import BackTopDemoComponent from './back-top-demo'
 import DevUIApiComponent from '../../shared/devui-api/devui-api'
 
+import ApiCn from '../doc/api-cn.md'
+import ApiEn from '../doc/api-en.md'
 const routes = [
   { path: '',  redirectTo: 'demo' },
   { path: 'demo', component: BackTopDemoComponent},
   { path: 'api', component: DevUIApiComponent, meta: {
-    'zh-cn': '../doc/api-cn.md',
-    'en-us': '../doc/api-en.md'
+    'zh-cn': ApiCn,
+    'en-us': ApiEn
   }}
 ]
 
---
+++ scripts/delete-old-api.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+const shelljs = require('shelljs')
+const { COMPONENTS } = require('./const')
+
+const targetPath = path.resolve(__dirname, '../devui')
+
+function deleteOldApi(targetPath) {
+  fs.readdir(targetPath, function(targetPathError, targetComponentFolder) {
+    if (targetPathError) {
+      console.error(targetPathError)
+      return
+    }
+    
+    targetComponentFolder
+    .filter(doc => COMPONENTS.includes(doc))
+    .forEach((componentName) => {
+      const targetApiPath = path.resolve(targetPath, componentName, 'api')
+
+      // 在组件目录下创建 doc 目录
+      if (fs.existsSync(targetApiPath)) {
+        console.log('targetApiPath:', targetApiPath)
+        shelljs.rm('-rf', targetApiPath);
+      }
+    })
+  })
+}
+
+deleteOldApi(targetPath)
