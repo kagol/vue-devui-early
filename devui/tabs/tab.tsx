a099b6132e0a6e9e0cb13d3fb296d6de4a62197f
fangliang <907648506@qq.com>
2021-03-06 21:22:19 +0800

    feat(tabs): add tabs; All functions are implemented except 'slider' and 'beforeChange'hange'

--- devui/tabs/tab.tsx
+++ devui/tabs/tab.tsx
@@ -1,12 +1,35 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent, getCurrentInstance, inject, reactive } from 'vue'
+import { Tabs } from './tabs';
 
 export default defineComponent({
   name: 'd-tab',
   props: {
+    title: {
+      default: null,
+      type: [String, Number]
+    },
+    id: {
+      default: null,
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
   },
-  setup(props, ctx) {
+  setup(props, {slots}) {
+    const tabs = inject<Tabs>(
+      'tabs'
+    );
+    tabs.state.data.push(props);
     return () => {
-      return <div>{ctx.slots.default?.()}</div>
+      const content = tabs.state.showContent &&  tabs.state.active === props.id  ? (
+      <div class="devui-tab-content">
+          <div role="tabpanel" class="devui-tab-pane in active">
+            {slots.default()}
+        </div>
+      </div>): null;
+      return content
     }
   }
 })
--- src/components/app-demo-cell.vue
+++ src/components/app-demo-cell.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="main">
     <div class="docs-header">
-      <h1>{{ data.name || componentName }} {{ data.cnName }}</h1>
-      <div style="margin-bottom: 40px;">{{ data.description }}</div>
+      <h1>{{ data.name }} {{ data.cnName }}</h1>
+      <div style="margin-bottom: 40px">{{ data.description }}</div>
       <div v-if="data.tmw">
         <h3>何时使用</h3>
-        <div style="margin-bottom: 20px;" v-html="data.tmw"></div>
+        <div style="margin-bottom: 20px" v-html="data.tmw"></div>
       </div>
       <!-- TODO: d-tabs -->
-      <d-tabs :showContent="false" :activeTab="componentTab" @activeTabChange="activeTabChange($event)">
-        <d-tab id="demo" title="Demo" tabId="demo">
-          <router-link to="demo">Demo</router-link>
-        </d-tab>
-        <d-tab id="api" title="API" tabId="api">
-          <router-link to="api">API</router-link>
-        </d-tab>
+      <d-tabs
+        v-model="activeTab"
+        :showContent="false"
+        @activeTabChange="activeTabChange($event)"
+      >
+        <d-tab id="demo" title="demo" tabId="demo"> </d-tab>
+        <d-tab id="api" title="API" tabId="api"> </d-tab>
       </d-tabs>
     </div>
     <div class="examples-viewer">
@@ -27,9 +27,9 @@
 </template>
 
 <script>
-import ButtonDemo from '../../devui/button/demo/button-demo'
-import Tabs from '../../devui/tabs/tabs'
-import Tab from '../../devui/tabs/tab'
+import ButtonDemo from '../../devui/button/demo/button-demo';
+import Tabs from '../../devui/tabs/tabs';
+import Tab from '../../devui/tabs/tab';
 
 export default {
   name: 'app-demo-cell',
@@ -41,11 +41,17 @@ export default {
   data() {
     return {
       data: {},
-      componentName: ''
+      activeTab: 'demo',
     };
+  },
+  methods: {
+    activeTabChange(a) {
+      this.$router.push(a);
+    },
   },
   mounted() {
     this.data = this.$route.meta;
+    this.activeTab = /\/demo$/.test(this.$route.path) ? 'demo' : 'api';
   },
-}
+};
 </script>
