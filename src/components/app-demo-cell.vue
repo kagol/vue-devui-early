ec53d1e85e36320751216e9c4964eee479a2b37c
fangliang <907648506@qq.com>
2021-03-06 21:03:06 +0800

    feat(tabs): add tabs; All functions are implemented except 'slider' and 'beforeChange'

--- src/components/app-demo-cell.vue
+++ src/components/app-demo-cell.vue
@@ -1,47 +1,54 @@
 <template>
   <div class="main">
     <div class="docs-header">
-      <h1>{{ data.name || componentName }} {{ data.cnName }}</h1>
-      <div style="margin-bottom: 40px;">{{ data.description }}</div>
+      <h1>{{ data.name }} {{ data.cnName }}</h1>
+      <div style="margin-bottom: 40px">{{ data.description }}</div>
       <div v-if="data.tmw">
         <h3>何时使用</h3>
-        <div style="margin-bottom: 20px;" v-html="data.tmw"></div>
+        <div style="margin-bottom: 20px" v-html="data.tmw"></div>
       </div>
       <!-- TODO: d-tabs -->
-      <d-tabs :showContent="false" :activeTab="componentTab" @activeTabChange="activeTabChange($event)">
-        <d-tab id="demo" title="Demo" tabId="demo"></d-tab>
-        <d-tab id="api" title="API" tabId="api"></d-tab>
+      <d-tabs
+        v-model="activeTab"
+        :showContent="false"
+        @activeTabChange="activeTabChange($event)"
+      >
+        <d-tab id="demo" title="demo" tabId="demo"> </d-tab>
+        <d-tab id="api" title="API" tabId="api"> </d-tab>
       </d-tabs>
     </div>
     <div class="examples-viewer">
       <div class="examples-viewer-wrapper">
         <!-- TODO: Demo列表 -->
-        <d-button-demo></d-button-demo>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import DevUIButtonDemo from '../../devui/button/demo/button-demo.vue'
-import DevUITabs from '../../devui/tabs/tabs.vue'
-import DevUITab from '../../devui/tabs/tab.vue'
+import Tabs from '../../devui/tabs/tabs';
+import Tab from '../../devui/tabs/tab';
 
 export default {
   name: 'app-demo-cell',
   components: {
-    'd-button-demo': DevUIButtonDemo,
-    'd-tabs': DevUITabs,
-    'd-tab': DevUITab,
+    'd-tabs': Tabs,
+    'd-tab': Tab,
   },
   data() {
     return {
       data: {},
-      componentName: ''
+      activeTab: 'demo',
     };
+  },
+  methods: {
+    activeTabChange(a) {
+      this.$router.push(a);
+    },
   },
   mounted() {
     this.data = this.$route.meta;
+    this.activeTab = /\/demo$/.test(this.$route.path) ? 'demo' : 'api';
   },
-}
+};
 </script>
